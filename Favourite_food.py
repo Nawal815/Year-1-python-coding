caa5424069e0e8e3bc245ca0c13c18bd79d1d232
Nawal815 <nawalamanullah08@gmail.com>
2024-10-24 13:40:53 +0100

    Add files via upload

---
+++ Favourite_food.py
@@ -0,0 +1,15 @@
+#This program will ask the user to enter their favourite food, then assign it to a variable and display it 
+
+#--------------------------
+#Subprograms
+#--------------------------
+def favourite_food():
+    food = ()
+    
+ 
+    
+#--------------------------
+#Main program
+#--------------------------
+favourite_food = str(input("Enter your favourite food:")) 
+print(favourite_food)
