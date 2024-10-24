31a4c68f29062b4c0bbe9a63897faf607da3403b
Nawal815 <nawalamanullah08@gmail.com>
2024-10-24 13:19:38 +0100

    Add files via upload

---
+++ States_of_water.py
@@ -0,0 +1,23 @@
+﻿#States of water program
+
+#-----------------------------
+#Subprograms
+#-----------------------------
+def water_state(centigrade):
+    #Less than or equal to 0°.
+    if centigrade <=0:
+        return "Solid"
+    #Less than 100° but greater than 0°.
+    elif centigrade < 100:
+        return "liquid"
+    #100° or above.
+    else:
+        return "gaseous"
+
+#-----------------------------
+#Main Program
+#-----------------------------
+temperature = float(input("Enter the temperature in ℃: "))
+state = water_state(temperature)
+print("The water is in a", state, "state.")
+
