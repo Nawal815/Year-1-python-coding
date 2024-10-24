25a091446cf1a72e9c531b4c6dffd5beacf1f6b9
Nawal815 <nawalamanullah08@gmail.com>
2024-10-24 13:47:08 +0100

    Add files via upload

---
+++ Superhero_name_generator.py
@@ -0,0 +1,15 @@
+# A program to generate a superhero name
+
+# ----------------
+# Subprograms
+# ----------------
+
+  
+# ----------------
+# Main program
+# ----------------
+#Welcomes the user to the game
+print("Welcome to the Superhero Name Generator!")
+#stores useer response inside variables
+user_adjective = input("Enter an adjective (describing word): ")
+user_animal = input("Enter your favourite species of animal: ")
